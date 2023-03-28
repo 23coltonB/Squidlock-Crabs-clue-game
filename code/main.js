fbe4227cbad6ae45bbc448caa93326e54742b690
Colton Bennett <78919847+23coltonB@users.noreply.github.com>
2023-03-28 15:52:03 +0000

    Made a clambert ai, doors no longer work

--- code/main.js
+++ code/main.js
@@ -31,19 +31,24 @@ var suspect2 = false;
 var suspect3 = false;
 var suspect4 = false;
 var suspect5 = false;
+// bounderies
+
 // initialize context
-kaboom();
+kaboom({
+  background: [36, 36, 36],
+});
 
 // load assets
 loadSprite("bean", "sprites/bean.png")
-
+loadSprite("door", "sprites/door.png");
+loadSprite("butterfly", "sprites/butterfly.png");
 
 //sounds/music loading
 
 
 //actual game
-scene ('Title Screen', () => {
-  
+scene('Title Screen', () => {
+
   add([
     rect(width(), height()),
     color(0, 0, 0),
@@ -64,8 +69,163 @@ scene ('Title Screen', () => {
     findGuilty();
     go('Start');
   });
-  scene('Start', () => {
-    
+});
+
+scene('Start', () => {
+  add([
+    rect(width(), 20),
+    outline(4),
+    pos(0, height()),
+    origin("botleft"),
+    area(),
+    solid(),
+    color(50.2, 50.2, 50.2),
+  ]);
+
+  //sprites
+  const door1 = add([
+    sprite('door'),
+    pos(width() * 3 / 4, height() - 50),
+    area(),
+    origin("center"),
+    "door",
+  ]);
+
+  const player = add([
+    sprite("bean"),
+    pos(10, height() -40),
+    area(),
+    origin('topleft'),
+    body(),
+  ]);
+//clambert ai
+  const clambert = add([
+    sprite("butterfly"),
+    pos(15, height() -200),
+    area(),
+    solid(),
+    scale(3/8),
+  ]);
+
+  clambert.action(() => {
+    const vectorToPlayer = vec2(player.pos).sub(clambert.pos);
+    clambert.move(vectorToPlayer);
+  });
+
+  //controls
+  keyDown("left", () => {
+    player.move(-(200), 0);
+  });
+
+  keyDown("right", () => {
+    player.move((200), 0);
+  });
+  player.collides('door', () => {
+    if (keyIsPressed('e')) {
+      destroy(player);
+      go('building1');
+    }
+  });
+  //
+});
+
+scene('building1', () => {
+  add([
+    rect(600, height()),
+    outline(4),
+    pos((width() / 2), (height() / 2)),
+    origin("center"),
+    area(),
+    color(5, 5, 5),
+  ]);
+
+  add([
+    rect(20, height()),
+    pos((width() / 4), 0),
+    origin("center"),
+    area(),
+    body(),
+    color(50.2, 50.2, 50.2),
+  ]);
+
+  add([
+    rect(width(), 20),
+    outline(4),
+    pos(0, height()),
+    origin("botleft"),
+    area(),
+    solid(),
+    color(50.2, 50.2, 50.2),
+  ]);
+   // add the walls
+  add([
+    rect(width(), 20),
+    pos(0, height() - 20),
+    outline(4),
+    area(),
+    solid(),
+    color(50.2, 50.2, 50.2),
+  ]);
+  add([
+    rect(20, height()),
+    pos(0, 0),
+    outline(4),
+    area(),
+    solid(),
+    color(50.2, 50.2, 50.2),
+  ]);
+  add([
+    rect(20, height()),
+    pos(width() - 20, 0),
+    outline(4),
+    area(),
+    solid(),
+    color(50.2, 50.2, 50.2),
+  ]);
+
+  const door1 = add([
+    sprite('door'),
+    pos(width() * 2 / 4, height() - 50),
+    area(),
+    origin("center"),
+    "door",
+  ]);
+
+  const player = add([
+    sprite("bean"),
+    pos((width() * 2 / 4) + 100, height() - 50),
+    area(),
+    origin('topleft'),
+    body(),
+  ]);
+
+  //clambert ai
+  const clambert = add([
+    sprite("butterfly"),
+    pos(15, height() -200),
+    area(),
+    solid(),
+    scale(3/8),
+  ]);
+
+  clambert.action(() => {
+    const vectorToPlayer = vec2(player.pos).sub(clambert.pos);
+    clambert.move(vectorToPlayer);
+  });
+
+  //controls
+  keyDown("left", () => {
+    player.move(-(200), 0);
+  });
+
+  keyDown("right", () => {
+    player.move((200), 0);
+  });
+player.collides('door', () => {
+    if (keyIsPressed('e')) {
+      destroy(player);
+      go('Start');
+    }
   });
 });
 
@@ -73,98 +233,103 @@ go('Title Screen');
 
 // functions
 
-function findGuilty(){
+function findGuilty() {
   //which suspect is guilty
   var guiltyID = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
-  if (guiltyID == 1){
+  if (guiltyID == 1) {
     //character 1 is guilty
+    console.log("sus 1 picked");
     suspect1 = true;
-  }else if (guiltyID == 2){
+  } else if (guiltyID == 2) {
     suspect2 = true;
-  }else if (guiltyID == 3){
+    console.log("sus 2 picked");
+  } else if (guiltyID == 3) {
     suspect3 = true;
-  }else{
+    console.log("sus 3 picked");
+  } else {
     suspect4 = true;
+    console.log("sus 4 picked");
   }
   //clue id lookup
-  for (var i = 0; i <= 5; i++){
+  for (var i = 1; i <= 5; i++) {
     var clueID = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
-    if (clueID == 1 && clue1 != true){
+    if (clueID == 1 && clue1 != true) {
       //clue clue 1
-       clue1 = true;
-    }else if (clueID == 2 && clue2 != true){
-      
-       clue2 = true;
-    }else if (clueID == 3 && clue3 != true){
-
-       clue3 = true;
-    }else if (clueID == 4 && clue4 != true){
-
-       clue4 = true;
-    }else if (clueID == 5 && clue5 != true){
-
-       clue5 = true;
-    }else if (clueID == 6 && clue6 != true){
-
-       clue6 = true;
-    }else if (clueID == 7 && clue7 != true){
-
-       clue7 = true;
-    }else if (clueID == 8 && clue8 != true){
-
-       clue8 = true;
-    }else if (clueID == 9 && clue9 != true){
-
-       clue9 = true;
-    }else if (clueID == 10 && clue10 != true){
-
-       clue10 = true;
-    }else if (clueID == 11 && clue11 != true){
-
-       clue11 = true;
-    }else if (clueID == 12 && clue12 != true){
-
-       clue12 = true;
-    }else if (clueID == 13 && clue13 != true){
-
-       clue13 = true;
-    }else if (clueID == 14 && clue14 != true){
-
-       clue14 = true;
-    }else if (clueID == 15 && clue15 != true){
-
-       clue15 = true;
-    }else if (clueID == 16 && clue16 != true){
-
-       clue16 = true;
-    }else if (clueID == 17 && clue17 != true){
-
-       clue17 = true;
-    }else if (clueID == 18 && clue18 != true){
-
-       clue18 = true;
-    }else if (clueID == 19 && clue19 != true){
-
-       clue19 = true;
-    }else if (clueID == 20 && clue20 != true){
-
-       clue20 = true;
-    }else if (clueID == 21 && clue21 != true){
-
-       clue21 = true;
-    }else if (clueID == 22 && clue22 != true){
-
-       clue22 = true;
-    }else if (clueID == 23 && clue23 != true){
-
-       clue23 = true;
-    }else if (clueID == 24 && clue24 != true){
-
-       clue24 = true;
-    }else if (clueID == 25 && clue25 != true){
-      
-       clue25 = true;
-    }else{
+      clue1 = true;
+      console.log('clue 1 picked');
+    } else if (clueID == 2 && clue2 != true) {
+      console.log('clue 2 picked');
+      clue2 = true;
+    } else if (clueID == 3 && clue3 != true) {
+      console.log('clue 3 picked');
+      clue3 = true;
+    } else if (clueID == 4 && clue4 != true) {
+      console.log('clue 4 picked');
+      clue4 = true;
+    } else if (clueID == 5 && clue5 != true) {
+      console.log('clue 5 picked');
+      clue5 = true;
+    } else if (clueID == 6 && clue6 != true) {
+      console.log('clue 6 picked');
+      clue6 = true;
+    } else if (clueID == 7 && clue7 != true) {
+      console.log('clue 7 picked');
+      clue7 = true;
+    } else if (clueID == 8 && clue8 != true) {
+      console.log('clue 8 picked');
+      clue8 = true;
+    } else if (clueID == 9 && clue9 != true) {
+      console.log('clue 9 picked');
+      clue9 = true;
+    } else if (clueID == 10 && clue10 != true) {
+      console.log('clue 10 picked');
+      clue10 = true;
+    } else if (clueID == 11 && clue11 != true) {
+      console.log('clue 11 picked');
+      clue11 = true;
+    } else if (clueID == 12 && clue12 != true) {
+      console.log('clue 12 picked');
+      clue12 = true;
+    } else if (clueID == 13 && clue13 != true) {
+      console.log('clue 13 picked');
+      clue13 = true;
+    } else if (clueID == 14 && clue14 != true) {
+      console.log('clue 14 picked');
+      clue14 = true;
+    } else if (clueID == 15 && clue15 != true) {
+      console.log('clue 15 picked');
+      clue15 = true;
+    } else if (clueID == 16 && clue16 != true) {
+      console.log('clue 16 picked');
+      clue16 = true;
+    } else if (clueID == 17 && clue17 != true) {
+      console.log('clue 17 picked');
+      clue17 = true;
+    } else if (clueID == 18 && clue18 != true) {
+      console.log('clue 18 picked');
+      clue18 = true;
+    } else if (clueID == 19 && clue19 != true) {
+      console.log('clue 19 picked');
+      clue19 = true;
+    } else if (clueID == 20 && clue20 != true) {
+      console.log('clue 20 picked');
+      clue20 = true;
+    } else if (clueID == 21 && clue21 != true) {
+      console.log('clue 21 picked');
+      clue21 = true;
+    } else if (clueID == 22 && clue22 != true) {
+      console.log('clue 22 picked');
+      clue22 = true;
+    } else if (clueID == 23 && clue23 != true) {
+      console.log('clue 23 picked');
+      clue23 = true;
+    } else if (clueID == 24 && clue24 != true) {
+      console.log('clue 24 picked');
+      clue24 = true;
+    } else if (clueID == 25 && clue25 != true) {
+      console.log('clue 25 picked');
+      clue25 = true;
+    } else {
       i--;
       console.log("duplicate item found, finding another...");
     }
--- dist/game.js
+++ dist/game.js
@@ -2942,8 +2942,12 @@ vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
   var suspect2 = false;
   var suspect3 = false;
   var suspect4 = false;
-  no();
+  no({
+    background: [36, 36, 36]
+  });
   loadSprite("bean", "sprites/bean.png");
+  loadSprite("door", "sprites/door.png");
+  loadSprite("butterfly", "sprites/butterfly.png");
   scene("Title Screen", () => {
     add([
       rect(width(), height()),
@@ -2963,71 +2967,237 @@ vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
     ]);
     keyPress("enter", () => {
       findGuilty();
+      go("Start");
+    });
+  });
+  scene("Start", () => {
+    add([
+      rect(width(), 20),
+      outline(4),
+      pos(0, height()),
+      origin("botleft"),
+      area(),
+      solid(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    const door1 = add([
+      sprite("door"),
+      pos(width() * 3 / 4, height() - 50),
+      area(),
+      origin("center"),
+      "door"
+    ]);
+    const player = add([
+      sprite("bean"),
+      pos(10, height() - 40),
+      area(),
+      origin("topleft"),
+      body()
+    ]);
+    const clambert = add([
+      sprite("butterfly"),
+      pos(15, height() - 200),
+      area(),
+      solid(),
+      scale(3 / 8)
+    ]);
+    clambert.action(() => {
+      const vectorToPlayer = vec2(player.pos).sub(clambert.pos);
+      clambert.move(vectorToPlayer);
+    });
+    keyDown("left", () => {
+      player.move(-200, 0);
+    });
+    keyDown("right", () => {
+      player.move(200, 0);
+    });
+    player.collides("door", () => {
+      if (keyIsPressed("e")) {
+        destroy(player);
+        go("building1");
+      }
+    });
+  });
+  scene("building1", () => {
+    add([
+      rect(600, height()),
+      outline(4),
+      pos(width() / 2, height() / 2),
+      origin("center"),
+      area(),
+      color(5, 5, 5)
+    ]);
+    add([
+      rect(20, height()),
+      pos(width() / 4, 0),
+      origin("center"),
+      area(),
+      body(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    add([
+      rect(width(), 20),
+      outline(4),
+      pos(0, height()),
+      origin("botleft"),
+      area(),
+      solid(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    add([
+      rect(width(), 20),
+      pos(0, height() - 20),
+      outline(4),
+      area(),
+      solid(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    add([
+      rect(20, height()),
+      pos(0, 0),
+      outline(4),
+      area(),
+      solid(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    add([
+      rect(20, height()),
+      pos(width() - 20, 0),
+      outline(4),
+      area(),
+      solid(),
+      color(50.2, 50.2, 50.2)
+    ]);
+    const door1 = add([
+      sprite("door"),
+      pos(width() * 2 / 4, height() - 50),
+      area(),
+      origin("center"),
+      "door"
+    ]);
+    const player = add([
+      sprite("bean"),
+      pos(width() * 2 / 4 + 100, height() - 50),
+      area(),
+      origin("topleft"),
+      body()
+    ]);
+    const clambert = add([
+      sprite("butterfly"),
+      pos(15, height() - 200),
+      area(),
+      solid(),
+      scale(3 / 8)
+    ]);
+    clambert.action(() => {
+      const vectorToPlayer = vec2(player.pos).sub(clambert.pos);
+      clambert.move(vectorToPlayer);
+    });
+    keyDown("left", () => {
+      player.move(-200, 0);
+    });
+    keyDown("right", () => {
+      player.move(200, 0);
+    });
+    player.collides("door", () => {
+      if (keyIsPressed("e")) {
+        destroy(player);
+        go("Start");
+      }
     });
   });
   go("Title Screen");
   function findGuilty() {
     var guiltyID = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
     if (guiltyID == 1) {
+      console.log("sus 1 picked");
       suspect1 = true;
     } else if (guiltyID == 2) {
       suspect2 = true;
+      console.log("sus 2 picked");
     } else if (guiltyID == 3) {
       suspect3 = true;
+      console.log("sus 3 picked");
     } else {
       suspect4 = true;
+      console.log("sus 4 picked");
     }
-    for (var i = 0; i <= 5; i++) {
+    for (var i = 1; i <= 5; i++) {
       var clueID = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
       if (clueID == 1 && clue1 != true) {
         clue1 = true;
+        console.log("clue 1 picked");
       } else if (clueID == 2 && clue2 != true) {
+        console.log("clue 2 picked");
         clue2 = true;
       } else if (clueID == 3 && clue3 != true) {
+        console.log("clue 3 picked");
         clue3 = true;
       } else if (clueID == 4 && clue4 != true) {
+        console.log("clue 4 picked");
         clue4 = true;
       } else if (clueID == 5 && clue5 != true) {
+        console.log("clue 5 picked");
         clue5 = true;
       } else if (clueID == 6 && clue6 != true) {
+        console.log("clue 6 picked");
         clue6 = true;
       } else if (clueID == 7 && clue7 != true) {
+        console.log("clue 7 picked");
         clue7 = true;
       } else if (clueID == 8 && clue8 != true) {
+        console.log("clue 8 picked");
         clue8 = true;
       } else if (clueID == 9 && clue9 != true) {
+        console.log("clue 9 picked");
         clue9 = true;
       } else if (clueID == 10 && clue10 != true) {
+        console.log("clue 10 picked");
         clue10 = true;
       } else if (clueID == 11 && clue11 != true) {
+        console.log("clue 11 picked");
         clue11 = true;
       } else if (clueID == 12 && clue12 != true) {
+        console.log("clue 12 picked");
         clue12 = true;
       } else if (clueID == 13 && clue13 != true) {
+        console.log("clue 13 picked");
         clue13 = true;
       } else if (clueID == 14 && clue14 != true) {
+        console.log("clue 14 picked");
         clue14 = true;
       } else if (clueID == 15 && clue15 != true) {
+        console.log("clue 15 picked");
         clue15 = true;
       } else if (clueID == 16 && clue16 != true) {
+        console.log("clue 16 picked");
         clue16 = true;
       } else if (clueID == 17 && clue17 != true) {
+        console.log("clue 17 picked");
         clue17 = true;
       } else if (clueID == 18 && clue18 != true) {
+        console.log("clue 18 picked");
         clue18 = true;
       } else if (clueID == 19 && clue19 != true) {
+        console.log("clue 19 picked");
         clue19 = true;
       } else if (clueID == 20 && clue20 != true) {
+        console.log("clue 20 picked");
         clue20 = true;
       } else if (clueID == 21 && clue21 != true) {
+        console.log("clue 21 picked");
         clue21 = true;
       } else if (clueID == 22 && clue22 != true) {
+        console.log("clue 22 picked");
         clue22 = true;
       } else if (clueID == 23 && clue23 != true) {
+        console.log("clue 23 picked");
         clue23 = true;
       } else if (clueID == 24 && clue24 != true) {
+        console.log("clue 24 picked");
         clue24 = true;
       } else if (clueID == 25 && clue25 != true) {
+        console.log("clue 25 picked");
         clue25 = true;
       } else {
         i--;
